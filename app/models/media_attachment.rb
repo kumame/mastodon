b76d95861317784a0fc7705ceb2bdc39c05b4b8c
kumame <enchant@vip.qq.com>
2021-02-09 20:55:22 +0800

    20210209

--- app/models/media_attachment.rb
+++ app/models/media_attachment.rb
@@ -153,7 +153,7 @@ class MediaAttachment < ApplicationRecord
   }.freeze
 
   IMAGE_LIMIT = 2.megabytes
-  VIDEO_LIMIT = 20.megabytes
+  VIDEO_LIMIT = 4.megabytes
 
   MAX_VIDEO_MATRIX_LIMIT = 2_304_000 # 1920x1200px
   MAX_VIDEO_FRAME_RATE   = 60 # 帧率
